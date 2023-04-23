b16b4ae578cc3d9cae34a8c3a4b65493fdc0ab23
Azubuikeleo13 <azubuikeleo13@gmail.com>
2023-04-23 22:50:46 +0100

    Correction

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     width = 0
     height = 0
 
@@ -21,7 +21,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
