49893630310b06e05219409fc23216b529b92039
Azubuikeleo13 <azubuikeleo13@gmail.com>
2023-04-23 23:14:02 +0100

    Correction

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -17,7 +17,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return (self.width * self.width)
+        return (self.width * self.height)
 
     def permiter_of_my_square(self):
         """ Documentation """
